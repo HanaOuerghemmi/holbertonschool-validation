a562f8d0e8e5a8a8cd1c62a7083afbfb9f684681
HanaOuerghemmi <5004@holbertonschool.com>
2023-01-06 15:46:03 +0100

    hugo && makefile 4 sh

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-apt-get update && apt-get install -y git make curl build-essential
+
+## Install Hugo && make
+
+apt-get update && apt-get install -y hugo make
+
+## Using Curl to downlond hugo.deb
+
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt-get install ./hugo.deb
+
+## build the site 
+make build
 
