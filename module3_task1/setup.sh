495f7ed57d6ebaa028360822cc4a2befba5ff8dc
HanaOuerghemmi <5004@holbertonschool.com>
2023-01-06 20:48:39 +0100

    worflows

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make
-make build
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt-get install ./hugo.deb
